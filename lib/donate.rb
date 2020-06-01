ffde24a53c29091590606a9b79157c0ccd196edb
marcusLau <lau.marcus76@gmail.com>
2020-06-01 15:55:08 -0700

    Working initial baseline

--- lib/donate.rb
+++ lib/donate.rb
@@ -1,2 +1,2 @@
-require_relative "donate/version"
+require_relative "./donate/version"
 require_relative './donate/cli'
--- lib/donate/cli.rb
+++ lib/donate/cli.rb
@@ -1,7 +1,12 @@
 class Donate::CLI
 
     def call
-        puts "hello"
+        puts "Where to Donate"
+        list_funds
+    end
+
+    def list_funds 
+        
     end
 
 end
